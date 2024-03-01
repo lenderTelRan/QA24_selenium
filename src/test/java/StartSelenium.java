fe6fc86f38813dcc984af85fe8d03e9b54b0c87a
lenderTelRan <lender.vladimir@gmail.com>
2024-03-01 19:48:20 +0200

    first commit

---
+++ src/test/java/StartSelenium.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class StartSelenium {
+    WebDriver wd;
+
+    @Test
+    public void test() {
+        wd = new ChromeDriver();
+//        wd.get("https://telranedu.web.app/home");  // не запоминает историю переходов по сайту
+        wd.navigate().to("https://telranedu.web.app/home"); // запоминает историю переходов по сайту
+    }
+}
