4da5e9f0f198cd3315a434ec1c2c63dca10ead79
lenderTelRan <lender.vladimir@gmail.com>
2024-03-02 12:12:41 +0200

    new class loginTests

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,30 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+    @BeforeClass
+    public void preCondition() {
+        // open browser
+    }
+
+    @Test
+    public void loginSuccess() {
+
+    }
+
+    @Test
+    public void loginWrongEmail() {
+
+    }
+
+    @Test
+    public void loginWrongPassword() {
+
+    }
+
+    @AfterClass
+    public void postCondition() {
+        // close browser
+    }
+}
